5e7247038637e3b9f5d840e1d3e4901f0c34d510
Sebastian <sebastianszachnowicz@gmail.com>
2018-02-01 15:01:27 +0100

    Initial commit

---
+++ app/src/main/java/com/szachnowicz/simplyshopapp/presenter/MainActivityPresenter.java
@@ -0,0 +1,67 @@
+package com.szachnowicz.simplyshopapp.presenter;
+
+import android.content.Context;
+import android.graphics.Color;
+
+import com.nightonke.boommenu.BoomButtons.HamButton;
+import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.szachnowicz.simplyshopapp.R;
+
+/**
+ * Created by Sebastian on 2018-02-01.
+ */
+
+public class MainActivityPresenter {
+    private Context context;
+
+    public MainActivityPresenter(Context context) {
+        this.context = context;
+    }
+
+    public void initMenu(BoomMenuButton boomMenuButton) {
+
+        boomMenuButton.addBuilder(getSearchInShopBulider());
+        boomMenuButton.addBuilder(getYourOrderBulider());
+        boomMenuButton.addBuilder(getOrderHistoryBulider());
+        boomMenuButton.addBuilder(getOrderHistoryBulider());
+
+
+    }
+
+    private HamButton.Builder getOrderHistoryBulider() {
+        HamButton.Builder builder = new HamButton.Builder();
+        builder.normalColor(Color.parseColor("#CFB53B"))
+                .normalTextRes(R.string.orderHistory)
+                .subNormalTextRes(R.string.orderHistorySub)
+                .normalImageRes(R.drawable.ic_history)
+                .listener(index -> {
+                })
+                .build(context);
+        return builder;
+    }
+
+    private HamButton.Builder getYourOrderBulider() {
+        HamButton.Builder builder = new HamButton.Builder();
+        builder.normalColor(Color.parseColor("#CFB53B"))
+                .normalText(context.getString(R.string.yourOrder))
+                .subNormalTextRes(R.string.yourOrderSub)
+                .normalImageRes(R.drawable.ic_recent_order)
+                .listener(index -> {
+                })
+                .build(context);
+        return builder;
+    }
+
+    private HamButton.Builder getSearchInShopBulider() {
+        HamButton.Builder builder = new HamButton.Builder();
+        builder.normalColor(Color.parseColor("#CFB53B"))
+                .normalText(context.getString(R.string.searchInShop))
+                .subNormalTextRes(R.string.searchInShopSub)
+                .normalImageRes(R.drawable.ic_search)
+                .listener(index -> {
+                })
+                .build(context);
+        return builder;
+    }
+}
